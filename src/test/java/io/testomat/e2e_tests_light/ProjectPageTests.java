198fe9758a330cad287d8e4cd4465d5c1009014d
artemillin <artem.illin@amo.tech>
2026-03-07 16:12:30 +0200

    add Sign Out Test add Create Project Test

--- src/test/java/io/testomat/e2e_tests_light/ProjectPageTests.java
+++ src/test/java/io/testomat/e2e_tests_light/ProjectPageTests.java
@@ -1,13 +1,17 @@
 package io.testomat.e2e_tests_light;
 
 import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class ProjectPageTests {
+
     @Test
     public void userCanFindProjectWithTests() {
         open("https://app.testomat.io/");
@@ -22,7 +26,23 @@ public class ProjectPageTests {
         $(Selectors.byText("test Project Artem")).click();
         sleep(10000);
         $("h2").shouldHave(text("test Project Artem"));
+    }
 
+    @Test
+    void userCanCreateEmptyProject() {
+        open("https://app.testomat.io/");
+
+        $("#content-desktop #user_email").setValue("ilinartem293@gmail.com");
+        $("#content-desktop #user_password").setValue("ilinartem441$AQACourse");
+        $("#content-desktop #user_remember_me").click();
+        $("#content-desktop [name=\"commit\"]").click();
+
+        $(".common-flash-success").shouldBe(visible);
+        $(".common-btn-primary").click();
+        $("[placeholder='My Project']").setValue("Project created with automation");
+        $("#project-create-btn .common-btn-primary").click();
+        $("#welcometotestomatio").shouldBe(visible);
+        $("#welcometotestomatio").shouldHave(text("Welcome to Testomat.io"));
 
     }
 }
